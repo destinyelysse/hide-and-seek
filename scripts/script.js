8bae396881eb4862c33b656c1b47c78cff9d71f0
Destiny Serna <destinyeserna@gmail.com>
2021-11-21 17:54:10 -0600

    refactored so many game tiles can be made

--- scripts/script.js
+++ scripts/script.js
@@ -1,66 +1,84 @@
-let spotImage1 = document.getElementById("spot1");
-let spotImage2 = document.getElementById("spot2");
-let spotImage3 = document.getElementById("spot3");
+let totalSpots = 50;
 
-const pizzaCatImagePath = "./images/pizza-cat.png";
+const setupSpots = (spotsToMake) => {
+    let spotArray = [];
+
+    for (let i=1; i<=spotsToMake; i++ ){
+        let newSpot = document.createElement("img");
+        newSpot.src = "./images/spot.png";
+        newSpot.id = "spot"+i;
+        newSpot.className = "spot";
+        spotArray.push(newSpot);
+    }
+
+    return spotArray;
+}
+
+const spots = setupSpots(totalSpots);
+
+const pizzaCatPath = "./images/pizza-cat.png";
 const doryInABoxPath = "./images/dory-in-a-box.png";
 const doryScratcherPath = "./images/dory-scratcher.png";
 
-spotImage1.onclick = () => {
-    spotImage1.src = foundDory1;
-}
+const setupDories = (totalDories) => {
 
-spotImage2.onclick = () => {
-    spotImage2.src = foundDory2;
-}
+    let srcs = [];
 
-spotImage3.onclick = () => {
-    spotImage3.src = foundDory3;
-}
-
-let foundDory1;
-let foundDory2;
-let foundDory3;
-
-const randomDoryLocationGenerator = () => {
-    let pizzaCatLocation = Math.floor(Math.random() * 3);
+    let pizzaCatLocation = Math.floor(Math.random() * (totalDories - 1));
     let doryScratcherLocation = pizzaCatLocation;
 
     while (doryScratcherLocation === pizzaCatLocation){
-        doryScratcherLocation = Math.floor(Math.random() * 2);
+        doryScratcherLocation = Math.floor(Math.random() * (totalDories - 1));
     }
 
-    if (pizzaCatLocation == 0){
-        if (doryScratcherLocation == 1){
-            foundDory1 = pizzaCatImagePath;
-            foundDory2 = doryScratcherPath;
-            foundDory3 = doryInABoxPath;
+    for (let i = 0; i<totalDories; i++){
+        if (i === pizzaCatLocation){
+            srcs.push(pizzaCatPath);
+        } else if (i=== doryScratcherLocation){
+            srcs.push(doryScratcherPath);
         } else {
-            foundDory1 = pizzaCatImagePath;
-            foundDory2 = doryInABoxPath;
-            foundDory3 = doryScratcherPath;
+            srcs.push(doryInABoxPath);
         }
-    } else if (pizzaCatLocation == 1){
-        if (doryScratcherLocation = 0){
-            foundDory1 = doryScratcherPath;
-            foundDory2 = pizzaCatImagePath;
-            foundDory3 = doryInABoxPath;
-        } else {
-            foundDory1 = doryInABoxPath;
-            foundDory2 = pizzaCatImagePath;
-            foundDory3 = doryScratcherPath;
+    }
+
+    return srcs;
+}
+
+let dories = setupDories(totalSpots);
+
+const assignDories = (spots, dories) => {
+    if (spots.length != dories.length){
+        console.log("spots and dories different lengths.")
+    }
+
+    for (let i = 0; i< spots.length; i++){
+        let spot = spots[i];
+        let dory = dories[i];
+        spot.onclick = () => {
+            console.log("old class");
+            console.log(spot.class)
+            spot.src = dory;
+            console.log(spot.class)
         }
-    } else {
-        if (doryScratcherLocation == 0){
-            foundDory1 = doryScratcherPath;
-            foundDory2 = doryInABoxPath;
-            foundDory3 = pizzaCatImagePath;
-        } else {
-            foundDory1 = doryInABoxPath;
-            foundDory2 = doryScratcherPath;
-            foundDory3 = pizzaCatImagePath;
-        }
+    }
+
+    return spots;
+}
+
+const gameSpots = assignDories(spots, dories);
+
+function renderSpots(gameSpots){
+
+    let spotContainer = document.getElementById("spot-container");
+    console.log("got container");
+    console.log(spotContainer);
+
+    for (spot of gameSpots){
+        console.log(spot.class);
+        spotContainer.appendChild(spot);
+
     }
 }
 
-randomDoryLocationGenerator();
+renderSpots(gameSpots);
+
